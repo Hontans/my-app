b3dfae6197e649d0ed1971415dc6e4eb15322f28
Hontans <hontanssylvain@gmail.com>
2025-11-20 19:47:18 +0100

    chore: ajouter des règles ESLint pour désactiver les avertissements sur les types explicites et les variables inutilisées

--- components/LiquidEther.tsx
+++ components/LiquidEther.tsx
@@ -1,5 +1,8 @@
 'use client';
 
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
 import React, { useEffect, useRef } from 'react';
 import * as THREE from 'three';
 
